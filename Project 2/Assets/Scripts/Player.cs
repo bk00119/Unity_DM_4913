9cda28e29b3e49d2b1aaff5a23dcfb0c6cfb8ec1
Brian Kim <39502789+bk00119@users.noreply.github.com>
2023-11-11 01:16:50 -0500

    commented out the testing code

--- Project 2/Assets/Scripts/Player.cs
+++ Project 2/Assets/Scripts/Player.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player {
-    //public int[] stones = new int[4] { 4, 4, 4, 4 };
-    public int[] stones = new int[4] { 1, 1, 1, 1 }; // REMOVE THIS
+    public int[] stones = new int[4] { 4, 4, 4, 4 };
+    //public int[] stones = new int[4] { 1, 1, 1, 1 }; // REMOVE THIS
 
     public bool hasNoStoneOnBoard() {
         for (int i = 0; i < stones.GetLength(0); i++) {
